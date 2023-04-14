aebf83fa145d8c41c7b5a2ca519313e77083b1e6
Sailaxmi7599 <64842241+Sailaxmi7599@users.noreply.github.com>
2023-04-13 20:45:25 -0500

    Done

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+//Get the default connection
 var indexRouter = require('./routes/index');
-var StudentRouter = require('./routes/Student');
 var usersRouter = require('./routes/users');
+var StudentRouter = require('./routes/Student');
 var boardRouter = require('./routes/board');
+var resourceRouter = require('./routes/resource');
 var selectorRouter = require('./routes/selector');
-
+var Student = require("./models/Student");
+  
 var app = express();
 
 // view engine setup
@@ -22,11 +24,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var mongoose = require('mongoose');
+
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
+
+var db = mongoose.connection;
+
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Student',StudentRouter);
-app.use('/board', boardRouter);
-app.use('/selector', selectorRouter);
+app.use('/Student', StudentRouter);
+app.use('/Board', boardRouter);
+app.use('/Selector', selectorRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,5 +62,41 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// We can seed the collection if needed on
+async function recreateDB(){
+  // Delete everything
+  await Student.deleteMany();
+  let instance1 = new
+  Student({Stu_Name:"Sailaxmi", Stu_Age:22,Mail_Id:"laxmi@gmail.com"});
+
+  instance1.save().then( () => {
+    console.log('Everything went well');
+  }).catch( (e) => {
+    console.log('There was an error', e.message);
+  });
+
+  let instance2 = new
+  Student({Stu_Name:"Rakshitha", Stu_Age:22,Mail_Id:"raksg@hmail.com"});
+
+  instance2.save().then( () => {
+    console.log('Everything went well');
+  }).catch( (e) => {
+    console.log('There was an error', e.message);
+  });
+
+  let instance3 = new
+  Student({Stu_Name:"Shivani", Stu_Age:22,Mail_Id:"shivk@yahoo.com"});
+
+  instance3.save().then( () => {
+    console.log('Everything went well');
+  }).catch( (e) => {
+    console.log('There was an error', e.message);
+  });
+  }
+  let reseed = true;
+  if (reseed) { recreateDB();}
+
+
 
 module.exports = app;
---
+++ controllers/Student.js
@@ -0,0 +1,62 @@
+var Student = require('../models/Student');
+// List of all Costumes
+exports.Student_list = function(req, res) {
+ res.send('NOT IMPLEMENTED: Student list');
+};
+// for a specific Costume.
+exports.Student_detail = function(req, res) {
+ res.send('NOT IMPLEMENTED: Student detail: ' + req.params.id);
+};
+// Handle Costume create on POST.
+exports.Student_create_post = function(req, res) {
+ res.send('NOT IMPLEMENTED: Student create POST');
+};
+// Handle Costume delete form on DELETE.
+exports.Student_delete = function(req, res) {
+ res.send('NOT IMPLEMENTED: Student delete DELETE ' + req.params.id);
+};
+// Handle Costume update form on PUT.
+exports.Student_update_put = function(req, res) {
+ res.send('NOT IMPLEMENTED: Student update PUT' + req.params.id);
+};
+
+// List of all Student
+exports.Student_list = async function(req, res) {
+    try{
+    theStudent = await Student.find();
+    res.send(theStudent);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+
+   // VIEWS
+// Handle a show all view
+exports.Student_view_all_Page = async function(req, res) {
+    try{
+    theStudent = await Student.find();
+    res.render('Student', { title: 'Student Search Results', results: theStudent });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+   // Handle Costume create on POST.
+exports.Student_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Student();
+    document.Stu_Name = req.body.Stu_Name;
+    document.Stu_Age = req.body.Stu_Age;
+    document.Mail_Id = req.body.Mail_Id;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
