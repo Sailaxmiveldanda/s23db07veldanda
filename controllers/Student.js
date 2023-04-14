bc8564cf168b325eb9d45ddab4c12c543e8bd90e
Sailaxmi7599 <64842241+Sailaxmi7599@users.noreply.github.com>
2023-04-14 11:54:12 -0500

    Upadted controllers

--- controllers/Student.js
+++ controllers/Student.js
@@ -4,9 +4,16 @@ exports.Student_list = function(req, res) {
  res.send('NOT IMPLEMENTED: Student list');
 };
 // for a specific Costume.
-exports.Student_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Student detail: ' + req.params.id);
-};
+exports.Student_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Student.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
 // Handle Costume create on POST.
 exports.Student_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: Student create POST');
@@ -16,10 +23,28 @@ exports.Student_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Student delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.Student_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Student update PUT' + req.params.id);
+//exports.Student_update_put = function(req, res) {
+ //res.send('NOT IMPLEMENTED: Student update PUT' + req.params.id);
+ // Handle Costume update form on PUT.
+exports.Student_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Student.findById( req.params.id)
+// Do updates of properties
+if(req.body.Stu_Name)
+toUpdate.Stu_Name = req.body.Stu_Name;
+if(req.body.Stu_Age) toUpdate.Stu_Age = req.body.Stu_Age;
+if(req.body.Mail_Id) toUpdate.Mail_Id = req.body.Mail_Id;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
-
 // List of all Student
 exports.Student_list = async function(req, res) {
     try{
