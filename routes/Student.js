1b9600812d09bce60a2d326c2d43006ab1761a9e
Sailaxmi7599 <64842241+Sailaxmi7599@users.noreply.github.com>
2023-04-19 16:51:38 -0500

    Bonus

--- routes/Student.js
+++ routes/Student.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Student', { title: 'Search Results for Student' });
-});
-
-module.exports = router;
-var express = require('express');
 const Student_controlers= require('../controllers/Student');
-var router = express.Router();
-/* GET costumes */
-router.get('/', Student_controlers.Student_view_all_Page );
+/* GET home page. */
+router.get('/', Student_controlers.Student_view_all_Page);
+router.get('/detail', Student_controlers.Student_view_one_Page);
+/* GET create Student page */
+router.get('/create', Student_controlers.Student_create_Page);
+router.get('/update', Student_controlers.Student_update_Page);
+router.get('/delete', Student_controlers.Student_delete_Page);
 module.exports = router;
